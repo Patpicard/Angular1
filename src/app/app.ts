2e2f0dac175b40869b4a9c340571d825de74fb89
Patpicard <Patpicard@users.noreply.github.com>
2026-05-22 19:39:31 -0300

    Final con signal en la variable

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,5 +9,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('Título cambiado');
-  protected  variable: string = "->Esta es la variable<---";
+  protected  variable =  signal("->Esta es la variable<---");
+
+    //  EL CAMBIO: Usamos el constructor o ngOnInit
+  //  El instructivo no dice nada, si usar botón o cuando así que lo pongo acá
+  constructor() {
+    // Primero un Delay
+     
+    setTimeout(() => {
+      console.log("Cambiando valor...");
+      
+      this.variable.set("->Este es el cambio en la variable<---");
+      
+
+    }, 12000); // Tiempo para capturar la pantallang
+  }
 }
